267cab9bdb4f22700d1d337e920cd5a5a1926813
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-07-30 21:13:51 +0300

    construct HttpResponse structure

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct HttpRequest {
-    pub method: Method,
+    pub http_method: HttpMethod,
     pub version: Version,
     pub resource: Resource,
     pub headers: HashMap<String, String>,//e.g <Key: Host,  Value: localhost:3000> 
@@ -20,7 +20,7 @@ impl From<String> for HttpRequest {
     //then parsing each line to populate the `HttpRequest` fields.
     fn from(req: String) -> Self {
 
-        let mut parsed_method = Method::Uninitialized;
+        let mut parsed_method = HttpMethod::Uninitialized;
 
         let mut parsed_version = Version::VersionOne;
 
@@ -68,7 +68,7 @@ impl From<String> for HttpRequest {
         //HTTPRequest struct with the parsed values
         
         HttpRequest {
-             method: parsed_method, 
+             http_method: parsed_method, 
              version: parsed_version, 
              resource: parsed_resource, 
              headers: parsed_headers, 
@@ -83,8 +83,8 @@ impl From<String> for HttpRequest {
 //process the request line of incoming HTTP request
 //by splitting the request into three parts(method, resource, version)
 //transform the splitted part into more structured data types
-//Mrhod, Resource, and Version
-fn process_request_line(s: &str) -> (Method, Resource, Version) {
+//Method, Resource, and Version
+fn process_request_line(s: &str) -> (HttpMethod, Resource, Version) {
     
     //parse the request line into individual chunks
     //request line typically looks like:
@@ -92,7 +92,7 @@ fn process_request_line(s: &str) -> (Method, Resource, Version) {
     let mut request = s.split_whitespace();
 
     //Extract the HTTP method(e.g "GET") from first part of the request line
-    let method = request.next().unwrap();
+    let http_method = request.next().unwrap();
 
     //Extract the resource(URI/URL)(e.g /index.html) from the second part of the req
     let resource = request.next().unwrap();
@@ -101,7 +101,7 @@ fn process_request_line(s: &str) -> (Method, Resource, Version) {
     let version = request.next().unwrap();
 
     (
-        method.into(),
+        http_method.into(),
         Resource::Path(resource.to_string()),
         version.into(),
     )
@@ -126,7 +126,7 @@ fn process_header_line(s: &str) -> (String, String) {
 //`Method` specifies the allowed values(variants)
 //for HTTP Methods
 #[derive(Debug, PartialEq)]
-pub enum Method {
+pub enum HttpMethod {
     Get,
     Post,
     Uninitialized,
@@ -135,12 +135,12 @@ pub enum Method {
 
 //`FROM` trait converts the incoming string slice
 //into HttpRequest data structure
-impl From<&str> for Method {
+impl From<&str> for HttpMethod {
     fn from(value: &str) -> Self {
         match value {
-            "GET" => Method::Get,
-            "POST" => Method::Post,
-            _ => Method::Uninitialized,
+            "GET" => HttpMethod::Get,
+            "POST" => HttpMethod::Post,
+            _ => HttpMethod::Uninitialized,
         }
     }
 }
@@ -176,8 +176,8 @@ mod tests {
 
     #[test]
     fn test_method_into() {
-        let m: Method = "GET".into();
-        assert_eq!(m, Method::Get);
+        let m: HttpMethod = "GET".into();
+        assert_eq!(m, HttpMethod::Get);
     }
 
     #[test]
@@ -197,7 +197,7 @@ mod tests {
 
         let req: HttpRequest = s.into();
 
-        assert_eq!(Method::Get, req.method);
+        assert_eq!(HttpMethod::Get, req.http_method);
         assert_eq!(Version::VersionOne, req.version);
         assert_eq!(Resource::Path("/hello".to_string()), req.resource);
     }
--- http/src/httpresponse.rs
+++ http/src/httpresponse.rs
@@ -1,8 +1,45 @@
 use std::collections::HashMap;
 use std::io::Result;
 
-pub struct HttpResponse<'a> {
-    pub head: HashMap<String, String>,
-    pub body: String,
-    pub status_code: &'a str
+use crate::httprequest::Version;
+
+pub struct HttpResponse<'a, T> {
+    pub head: Parts<'a>,
+    pub body: T,
+}
+
+impl<'a, T: Default> Default for HttpResponse<'a, T> {
+    fn default() -> Self {
+        HttpResponse::new(T::default())
+    }
+}
+
+impl<'a, T> HttpResponse<'a, T> {
+    fn new(body: T) -> Self {
+         HttpResponse{
+            body,
+            head: Parts::new(),   
+        }
+        
+    }
+}
+
+pub struct Parts<'a> {
+    pub status_code : &'a str,
+    pub status_text: &'a str,
+    pub headers: HashMap<String, String>,
+    pub version: Version,
+}
+
+impl<'a> Parts<'a> {
+    fn new() -> Self {
+        let mut headers = HashMap::new();
+        headers.insert("Host".into(), "localhost".into());
+        Parts { 
+            status_code: "OK", 
+            status_text: "", 
+            headers, 
+            version: Version::Uninitialized,
+        }
+    }
 }
